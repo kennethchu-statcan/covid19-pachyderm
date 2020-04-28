a4cb3aa132ad0c602324e0fb0c4aaaf197a33007
Blair Drummond <blairdrummond@protonmail.com>
2020-04-27 21:52:42 -0400

    git version

--- docker/build_image.sh
+++ docker/build_image.sh
@@ -6,11 +6,9 @@ set -e
 
 test -z "$1" && echo Need version number && exit 1
 
-IMAGE="kenchu-wifr-analysis:$1"
+IMAGE="kenchu-wifr-git:$1"
 
-# cp -r ../../pipeline-test/image-loadData/src src
 docker build . -t $IMAGE > stdout.docker.build 2> stderr.docker.build
-# rm -rf src
 
 docker tag  $IMAGE k8scc01covidacr.azurecr.io/$IMAGE
 docker push        k8scc01covidacr.azurecr.io/$IMAGE
